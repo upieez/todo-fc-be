87b511cb51e97d0e631046e36ac7d73658eec82b
Samuel Huang <56812343+upieez@users.noreply.github.com>
2024-05-08 12:29:18 +0800

    update allowed hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG") == "True"
 
-ALLOWED_HOSTS = ["localhost", "vercel"]
+ALLOWED_HOSTS = ["localhost", "vercel", "todo-fc-be.onrender", "todo-fc-be.onrender.com"]
 
 
 # Application definition
